227056f27a4694f6b9677d063e6c5cc1e5c994f5
Gabriel Souza de Carvalho Lima <gabrielsclima.2004@gmail.com>
2023-03-25 00:18:41 -0300

    Troca de cor no botão da linha do tempo

--- sobre_nos/sobre_nos.js
+++ sobre_nos/sobre_nos.js
@@ -1,9 +1,17 @@
+let activeColor = "#06d39a", 
+    innactiveColor = "#8d9292";
+
 function ano_2014(){
     document.getElementById("camada1").style.visibility="visible";
     document.getElementById("camada2").style.visibility="hidden";
     document.getElementById("camada3").style.visibility="hidden";
     document.getElementById("camada4").style.visibility="hidden";
     document.getElementById("camada5").style.visibility="hidden";
+    document.getElementById("ano_2014").style.backgroundColor = activeColor;
+    document.getElementById("ano_2016").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2018").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2020").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2023").style.backgroundColor = innactiveColor;
 }
 function ano_2016(){
     document.getElementById("camada1").style.visibility="hidden";
@@ -11,6 +19,11 @@ function ano_2016(){
     document.getElementById("camada3").style.visibility="hidden";
     document.getElementById("camada4").style.visibility="hidden";
     document.getElementById("camada5").style.visibility="hidden";
+    document.getElementById("ano_2014").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2016").style.backgroundColor = activeColor;
+    document.getElementById("ano_2018").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2020").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2023").style.backgroundColor = innactiveColor;
 }
 function ano_2018(){
     document.getElementById("camada1").style.visibility="hidden";
@@ -18,6 +31,11 @@ function ano_2018(){
     document.getElementById("camada3").style.visibility="visible";
     document.getElementById("camada4").style.visibility="hidden";
     document.getElementById("camada5").style.visibility="hidden";
+    document.getElementById("ano_2014").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2016").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2018").style.backgroundColor = activeColor;
+    document.getElementById("ano_2020").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2023").style.backgroundColor = innactiveColor;
 }
 function ano_2020(){
     document.getElementById("camada1").style.visibility="hidden";
@@ -25,6 +43,11 @@ function ano_2020(){
     document.getElementById("camada3").style.visibility="hidden";
     document.getElementById("camada4").style.visibility="visible";
     document.getElementById("camada5").style.visibility="hidden";
+    document.getElementById("ano_2014").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2016").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2018").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2020").style.backgroundColor = activeColor;
+    document.getElementById("ano_2023").style.backgroundColor = innactiveColor;
 }
 function ano_2023(){
     document.getElementById("camada1").style.visibility="hidden";
@@ -32,4 +55,9 @@ function ano_2023(){
     document.getElementById("camada3").style.visibility="hidden";
     document.getElementById("camada4").style.visibility="hidden";
     document.getElementById("camada5").style.visibility="visible";
+    document.getElementById("ano_2014").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2016").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2018").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2020").style.backgroundColor = innactiveColor;
+    document.getElementById("ano_2023").style.backgroundColor = activeColor;
 }
